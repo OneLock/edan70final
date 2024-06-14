7a5bcbb1c1efff5ec9f28299475fbd2347d66721
Oneil Nelson <nellyb170@gmail.com>
2024-06-14 13:47:53 +0200

    Create fix_and_unzip.sh

---
+++ fix_and_unzip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Function to fix and unzip a multipart ZIP file
+fix_and_unzip() {
+    local input_zip="$1"
+    local output_dir="$2"
+
+    # Fix the multipart ZIP file using zip -FF
+    zip -FF "$input_zip" --out "${input_zip%.zip}_single.zip"
+
+    # Unzip the fixed single ZIP file
+    unzip "${input_zip%.zip}_single.zip" "$output_dir"
+}
+
+# Navigate to the directory containing the zipped files
+cd zipped_file/model_split
+
+# Fix and unzip data.zip
+fix_and_unzip data.zip ../../
+
+# Navigate to the models directory
+cd ../models
+
+# Fix and unzip model_split.zip
+fix_and_unzip model_split.zip ../../
