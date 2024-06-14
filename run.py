93b71e72c4ff25e312713e7d42c30a009108723e
Oneil Nelson <nellyb170@gmail.com>
2024-06-14 13:18:36 +0200

    Update run.py

--- run.py
+++ run.py
@@ -9,9 +9,6 @@ from scripts.Segmenter import Segmenter
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', action='store_true', help='Uggleupplagan')
-    parser.add_argument('-c', action='store_true', help='Running the classifier')
-    parser.add_argument('-e', action='store_true', help='Evaluating the classifier')
-    parser.add_argument('-m', action='store_true', help='Generating a map visualization')
     args = parser.parse_args()
 
     with open('settings.json') as f:
@@ -25,26 +22,6 @@ def main():
 
         sc = Scrapper(rootdir, edition, url)
         sg = Segmenter(_config['segmenter'])
-       
-
-    # if args.a:
-    #     print('Annotating selected')
-    #     annotator.get_and_save_coords(config)
-        
-    # if args.c:
-    #     print('Running the classifier selected')
-    #     classifier.run(config)
-
-    # if args.e:
-    #     print('Evaluating the classifier selected')
-    #     classifier.evaluate(config)
-
-    #if args.r:
-        #TODO print("Classifier REPL selected")
-
-    # if args.m:
-    #     print('Generating a visualization selected')
-    #     visualization.twod_map_coords(config)
 
     if not any(vars(args).values()):
         parser.print_help()
